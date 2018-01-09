a0ce7cfb337daba9979ad3398f933006d380546c
Gcauvet <contact@gcauvet-dev.com>
2018-01-09 17:48:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="description" content="File sorting">
-
-        <title>File sorting</title>
+        <meta name="description" content="List elements from directory">
 
 		<style>
 			body {
